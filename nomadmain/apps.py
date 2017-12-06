a994024e354b67ad00aebd23c397039c1e7f5acb
Jason Dilworth <hello@jasondilworth.co.uk>
2017-12-06 22:02:50 +0000

    Adding initial models

---
+++ nomadmain/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NomadmainConfig(AppConfig):
+    name = 'nomadmain'
---
+++ nomadmain/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+# Create your models here.
+
+
+#A member of the NOMAD team
+class TeamMember(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    iRacing_number = models.CharField(max_length=10)
+    profile_photo = models.FileField()
+    date_of_birth = models.DateField()
+    biography = models.TextField()
+    twitch_channel = models.CharField(max_length=30)
+    youtube_channel = models.CharField(max_length=30)
+    personal_site = models.CharField(max_length=255)
+
+
+#An Article on the blog/news
+class Article(models.Model):
+    title = models.CharField()
+    image = models.FileField()
+    content = models.TextField()
+    category = models.CharField() #TODO: Add categories class, this should be a foreign field of that
+    timestamp = models.DateTimeField(auto_now=True)
+    team_members = models.ManyToManyField(TeamMember) #for tagging team members
+    
+#A piece of media
+class Media(models.Model):
+    filename = models.FileField()
+    team_members = models.ManyToManyField(TeamMember) #for tagging team members
+    
+# A result of a team member
+class Result(models.Model):
+    team_member = models.ForeignKey(TeamMember, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    series = models.CharField()
+    circuit = models.CharField()
+    race_type = models.CharField()
+    start_pos = models.CharField()
+    finish_pos = models.CharField()
+    iRating_change = models.IntegerField()
+    
+class Progress(models.Model):
+    team_member = models.ForeignKey(TeamMember, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now=True)
+    iRating = models.IntegerField()
