77eb9e998e981af4d7b3245fa4c9bb628bdb98f5
Jason Dilworth <hello@jasondilworth.co.uk>
2018-01-19 19:36:35 +0000

    Mobile viewport fix

--- nomadmain/views.py
+++ nomadmain/views.py
@@ -8,6 +8,9 @@ import requests, random
 TWITCH_CLIENT_ID = "fz19mrp6fjwd6qd7cueailm1qqq6za"
 YOUTUBE_CLIENT_ID = "AIzaSyC8C1rkKpcwRP6yGRRSmHTXxiMP8LSJR3E"
 
+NOMAD_TWITCH = "nomadsimracing"
+NOMAD_YOUTUBE = "UCeO12ET3oRfa7zPcBUjj3rw"
+
 def index(request):
     all_users = User.objects.all()
     users = []
@@ -64,27 +67,44 @@ def static_page(request, slug):
     context = { 'page' : page }
     return render(request, 'nomadmain/static_page.html', context)
         
+def check_twitch(channel=NOMAD_TWITCH):
+    twitch_request = "https://api.twitch.tv/kraken/streams/"+channel+"?client_id="+TWITCH_CLIENT_ID
+    r = requests.get(twitch_request).json()
+    stream = r['stream']
+    return stream
+
+def check_youtube(channel=NOMAD_YOUTUBE):
+    youtube_request = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId="+channel+"&type=video&eventType=live&key="+YOUTUBE_CLIENT_ID
+    r = requests.get(youtube_request).json()
+    return r
 
 def checkStreams():
     team_members = TeamMember.objects.all()
     live_streams = []
-    for team_member in team_members:
-        if team_member.team_member:
-            #CHECK FOR TWITCH CHANNEL
-            if team_member.twitch_channel:
-                twitch_request = "https://api.twitch.tv/kraken/streams/"+team_member.twitch_channel+"?client_id="+TWITCH_CLIENT_ID
-                r = requests.get(twitch_request).json()
-                stream = r['stream']
-                if stream != None:
-                    live_streams.append({'team_member':team_member, 'embed':'https://player.twitch.tv/?channel='+team_member.twitch_channel })
-            
-            #CHECK FOR YOUTUBE CHNNEL
-            if team_member.youtube_channel:
-                youtube_request = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId="+team_member.youtube_channel+"&type=video&eventType=live&key="+YOUTUBE_CLIENT_ID
-                r = requests.get(youtube_request).json()
-                if len(r['items']) > 0:
-                    live_streams.append({'team_member':team_member, 'embed':'https://www.youtube.com/embed/'+r['items'][0]['id']['videoId']+'?rel=0'})
     
+    #check for NOMAD streams first
+    stream = check_twitch()
+    if stream != None:
+        live_streams.append({'team_member':'NOMAD', 'embed':'https://player.twitch.tv/?channel='+NOMAD_TWITCH })
+    r = check_youtube()
+    if len(r['items']) > 0:
+        live_streams.append({'team_member':'NOMAD', 'embed':'https://www.youtube.com/embed/'+r['items'][0]['id']['videoId']+'?rel=0'})
+    
+    if len(live_streams) == 0: #IF NOMAD STREAMS AREN'T LIVE, CHECK MEMBERS
+        for team_member in team_members:
+            if team_member.team_member:
+                #CHECK FOR TWITCH CHANNEL
+                if team_member.twitch_channel:
+                    stream = check_twitch(team_member.twitch_channel)
+                    if stream != None:
+                        live_streams.append({'team_member':team_member, 'embed':'https://player.twitch.tv/?channel='+team_member.twitch_channel })
+                
+                #CHECK FOR YOUTUBE CHNNEL
+                if team_member.youtube_channel:
+                    r = check_youtube(team_member.youtube_channel)
+                    if len(r['items']) > 0:
+                        live_streams.append({'team_member':team_member, 'embed':'https://www.youtube.com/embed/'+r['items'][0]['id']['videoId']+'?rel=0'})
+        
     if len(live_streams) > 0:
         return random.choice(live_streams)
     else:
